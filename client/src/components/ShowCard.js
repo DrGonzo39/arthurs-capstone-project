3e01b83aacededaa1246c05272d6c4e9aeddc373
Arthur Benware <artpolitic91@gmail.com>
2023-10-29 17:07:01 -0400

    add show functionality, need guidance to proceed

--- client/src/components/ShowCard.js
+++ client/src/components/ShowCard.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 
 function ShowCard({ show }) {
-    const [title, setTitle] = useState(show.title)
     const [rating, setRating] = useState(show.rating)
     const [date, setDate] = useState(show.date)
     const [errors, setErrors] = useState([])
 
     return (
         <div>
-          <h1>{title}</h1>
-          <h2>{date}</h2>
-          <p>{rating}</p> 
+        <h1>{show.title}</h1>
+        <h2>Starring: {show.artist}</h2>
+          <form id="show_info">
+            <p>Date:</p>
+            <input 
+            type="text"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            />
+            <p>Rating:</p>
+            <input 
+            type="text"
+            id="rating"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+            />
+            <button type="submit">Edit this show</button>
+          </form>
           <ul>
              {errors.map((err) => (
              <li key={err}>{err}</li>
--- client/src/components/VenueCard.js
+++ client/src/components/VenueCard.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { VenuesContext } from "../contexts/venues"
+import { ArtistsContext } from "../contexts/artists";
 import ShowCard from "./ShowCard";
 
 
@@ -8,7 +9,57 @@ function VenueCard({ venue }) {
     const [rating, setRating] = useState(0)
     const [date, setDate] = useState("")
     const [errors, setErrors] = useState([])
-    const { venues } = useContext(VenuesContext);
+    const { venues, setVenues } = useContext(VenuesContext);
+    const { artists, setArtists } = useContext(ArtistsContext);
+
+    function onAddShow(newShow) {
+        const artistToUpdate = artists.find((artist) => artist.id === newShow.artist_id)
+        const updatedArtistShows = [...artistToUpdate.shows, newShow]
+        const updatedArtist = {...artistToUpdate, shows: updatedArtistShows }
+        const updatedArtistsArray = artists.map((artist) => {
+            if(artist.id === updatedArtist.id){
+                return updatedArtist
+            }else{
+                return artist
+            }
+        })
+        setArtists(updatedArtistsArray)
+        const updatedVenueShows = [...venue.shows, newShow]
+        const updatedVenue = {...venue, shows: updatedVenueShows}
+        const updatedVenuesArray = venues.map((venue) => {
+            if(venue.id === updatedVenue.id){
+                return updatedVenue
+            }else{
+                return venue
+            }
+        })
+        setVenues(updatedVenuesArray)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/shows", {
+           method: "POST",
+           headers: {
+            "Content-type": "application/json"
+           },
+           body: JSON.stringify({
+            venue_id: venue.id,
+            title: title,
+            rating: rating,
+            date: date 
+           }),  
+        }).then((r) => {
+            setTitle("")
+            setRating("")
+            setDate("")
+            if(r.ok){
+                r.json().then((newShow) => onAddShow(newShow))
+            }else{
+                r.json().then((err) => setErrors(err.errors))
+            }
+        })
+    }
     
 
     return (
@@ -17,7 +68,7 @@ function VenueCard({ venue }) {
             <h1 id="venue_name">{venue.name}</h1>
             <h2 id="venue_location">{venue.location}</h2>
             <h3 id="upcoming_shows">Coming Soon at this Venue!</h3>
-            <form >
+            <form onSubmit={handleSubmit}>
             <input 
             type="text"
             id="title"
